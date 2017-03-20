49acfb61ee896dcb6d31dbed19d04fc6c040eb82
hsalemwalla <hsalemwalla@gmail.com>
2017-03-20 18:26:47 -0400

    Easy to change between datasets

--- draw_edge_map.py
+++ draw_edge_map.py
@@ -4,13 +4,26 @@ from dicom_to_numpy import DicomArray
 from xml_parser import *
 
 da = DicomArray()
-da.read_dicom("./NSCLC-Radiomics/")
 
-data = parse_xml()
+# uncomment the code for the dataset you want to use:
 
-figure = da.plot_dicom(77)
-# Uncomment for LIDC dataset with edges
-# x = data["readingSession"][0][0][0]["x"]
-# y = data["readingSession"][0][0][0]["y"]
-# z = data["readingSession"][0][0][0]["z"]
-# da.drawEdgeMap(figure, x,y)
+### NSCLC Dataset
+# da.read_dicom("./NSCLC-Radiomics/")
+# figure = da.plot_dicom(77)
+### -------------
+
+### LIDC Dataset
+da.read_dicom("./LIDC-IDRI/")
+xml_filename = "072.xml"
+figure = da.plot_dicom(77, False)
+data = parse_xml(xml_filename)
+if not data:
+    print "XML not parsed"
+else:
+    x = data["readingSession"][0][0][0]["x"]
+    y = data["readingSession"][0][0][0]["y"]
+    z = data["readingSession"][0][0][0]["z"]
+    da.drawEdgeMap(figure, x,y)
+### -------------
+
+
